0f6f44a99796f26f8ff7a152f8566f9711195cc8
Manish <39481751+Manish123Sharma@users.noreply.github.com>
2025-05-10 11:19:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,7 +10,19 @@ function convertToRoman(num) {
     };
 
   //your code here
+ if (num <= 0 || num > 100000) {
+        return "Input out of range";
+    }
+	let result = '';
 
+    for (let [symbol, value] of romanMap) {
+        while (num >= value) {
+            result += symbol;
+            num -= value;
+        }
+    }
+
+    return result;
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
